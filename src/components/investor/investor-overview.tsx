909653339c4eea8a52b8400a8a9d2c6435be5b47
Selorm Gawu <128249946+selormgm@users.noreply.github.com>
2025-08-07 19:44:15 +0000

    change

--- src/components/investor/investor-overview.tsx
+++ src/components/investor/investor-overview.tsx
@@ -1,108 +1,70 @@
-import { TrendingDown, TrendingUp } from "lucide-react";
-
-import { Badge } from "@/components/ui/badge";
+import {
+  Banknote,
+  CheckCircle,
+  TrendingUp,
+  Users,
+} from "lucide-react";
 import {
   Card,
   CardAction,
   CardDescription,
   CardFooter,
   CardHeader,
-  CardTitle,
 } from "@/components/ui/card";
 
 export function SectionCards() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-6 w-full text-[#158f20]">
-      <Card className="dark:bg-card p-5 h-[220px] flex flex-col justify-between">
+      <Card className="dark:bg-card p-5 h-[220px] flex flex-col justify-between shadow-md">
         <CardHeader>
           <CardDescription className="text-sm font-medium  text-[#158f20]">
             Total Investments
           </CardDescription>
-          <CardTitle className="text-4xl font-semibold mb-2 text-[#158f20]">
-            $1,250.00
-          </CardTitle>
           <CardAction className="text-[#158f20]">
-            <Badge variant="outline" className="text-[#158f20]">
-              <TrendingUp />
-              +12.5%
-            </Badge>
+            <Banknote size="38" />
           </CardAction>
         </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium text-[#158f20]">
-            Trending up this month <TrendingUp className="size-4" />
-          </div>
-          <div className="text-muted-foreground">
-            Investments for the last 6 months
-          </div>
+        <CardFooter className="text-4xl font-semibold mb-2 text-[#158f20]">
+          $1,250.00
         </CardFooter>
       </Card>
-      <Card className="dark:bg-card p-5 h-[220px] flex flex-col justify-between">
+      <Card className="dark:bg-card p-5 h-[220px] flex flex-col justify-between shadow-md">
         <CardHeader>
           <CardDescription className="text-sm font-medium  text-[#158f20]">
             New Farmers
           </CardDescription>
-          <CardTitle className="text-4xl font-semibold mb-2 text-[#158f20]">
-            1,234
-          </CardTitle>
           <CardAction className="text-[#158f20]">
-            <Badge variant="outline" className="text-[#158f20]">
-              <TrendingDown />
-              -20%
-            </Badge>
+            <Users size="38" />
           </CardAction>
         </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium text-[#158f20]">
-            Down 20% this period <TrendingDown className="size-4" />
-          </div>
-          <div className="text-muted-foreground">
-            Acquisition needs attention
-          </div>
+        <CardFooter className="text-4xl font-semibold mb-2 text-[#158f20]">
+          1,234
         </CardFooter>
       </Card>
-      <Card className="dark:bg-card p-5 h-[220px] flex flex-col justify-between">
+      <Card className="dark:bg-card p-5 h-[220px] flex flex-col justify-between shadow-md">
         <CardHeader>
           <CardDescription className="text-sm font-medium  text-[#158f20]">
             Active Farmers
           </CardDescription>
-          <CardTitle className="text-4xl font-semibold mb-2 text-[#158f20]">
-            45,678
-          </CardTitle>
           <CardAction className="text-[#158f20]">
-            <Badge variant="outline" className="text-[#158f20]">
-              <TrendingUp />
-              +12.5%
-            </Badge>
+            <CheckCircle size="38" />
           </CardAction>
         </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium text-[#158f20]">
-            Strong user retention <TrendingUp className="size-4" />
-          </div>
-          <div className="text-muted-foreground">Engagement exceed targets</div>
+        <CardFooter className="text-4xl font-semibold mb-2 text-[#158f20]">
+          45,678
         </CardFooter>
       </Card>
-      <Card className="dark:bg-card p-5 h-[220px] flex flex-col justify-between">
+      <Card className="dark:bg-card p-5 h-[220px] flex flex-col justify-between shadow-md">
         <CardHeader>
           <CardDescription className="text-sm font-medium  text-[#158f20]">
             Return On Investments
           </CardDescription>
-          <CardTitle className="text-4xl font-semibold mb-2 text-[#158f20]">
-            4.5%
-          </CardTitle>
           <CardAction className="text-[#158f20]">
-            <Badge variant="outline" className="text-[#158f20]">
-              <TrendingUp />
-              +4.5%
-            </Badge>
+            <TrendingUp size="38" />
           </CardAction>
         </CardHeader>
-        <CardFooter className="flex-col items-start gap-1.5 text-sm">
-          <div className="line-clamp-1 flex gap-2 font-medium text-[#158f20]">
-            Steady performance increase <TrendingUp className="size-4" />
-          </div>
-          <div className="text-muted-foreground">Meets growth projections</div>
+        <CardFooter className="text-4xl font-semibold mb-2 text-[#158f20]">
+          4.5%
         </CardFooter>
       </Card>
     </div>
